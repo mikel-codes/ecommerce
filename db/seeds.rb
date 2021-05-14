52aeab07f9a3c17b3105799c590ad2bec2352abb
smeez <gatez@yahoo.com>
2021-05-14 04:04:49 +0100

    adds product data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+    Product.create(
+        name: Faker::Lorem.word,
+        description: Faker::Lorem.sentence,
+        price: Faker::Number.decimal(l_digits: 2)
+    )
+end
