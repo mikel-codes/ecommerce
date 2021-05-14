153a44c0410374b3acbd53f1b49de7955238b014
smeez <gatez@yahoo.com>
2021-05-14 18:56:03 +0100

    renamed route for catalog#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :cart_lines
   resources :carts
-  get 'catalog/index'
+  get 'shop/products', to: "catalog#index"
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
