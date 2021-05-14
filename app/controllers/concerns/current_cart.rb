398e4154f1d069c4470291f6eee553d8122427d8
smeez <gatez@yahoo.com>
2021-05-14 18:55:40 +0100

    fixed name error for module

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,13 +1,13 @@
 module CurrentCart
+    extend ActiveSupport::Concern
     private
     #if no cart exist for a logged session, create one
     def set_cart_as_current_cart
         begin
             @cart = Cart.find(session[:cart_id])
-        rescue ActiveModel::RecordNotFound => e
+        rescue ActiveRecord::RecordNotFound => e
             @cart = Cart.create
             session[:cart_id] = @cart
         end
-
     end
 end
