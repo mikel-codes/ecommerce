37d1ffe2ea33e808fd405f041c8744835a916995
smeez <gatez@yahoo.com>
2021-05-15 01:42:00 +0100

    removed the begin end tags

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,11 +3,9 @@ module CurrentCart
     private
     #if no cart exists in the request sessions,create one
     def set_current_cart
-        begin
-            @cart = Cart.find(session[:cart_id])
-        rescue ActiveRecord::RecordNotFound
-            @cart = Cart.create
-            session[:cart_id] = @cart.id
-        end
+        @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+        @cart = Cart.create
+        session[:cart_id] = @cart.id
     end
 end
