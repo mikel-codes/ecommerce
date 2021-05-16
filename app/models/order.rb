ced0e01d72db18c8fa059f372ccb5f2666a6e678
smeez <gatez@yahoo.com>
2021-05-16 19:56:24 +0100

    adds validations for all fields

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
-    enum pay_types: {
+    enum payment_types: {
         "Check" => 0,
         "Credit Card" => 1,
         "Online Merchant" => 2
     }
+    validates :name, :email, :address, presence: true
+    validates :pay_type, inclusion: payment_types.keys
 end
