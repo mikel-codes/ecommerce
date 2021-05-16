b233dd99feac248dc114a3c3622d08cfd6f7f583
smeez <gatez@yahoo.com>
2021-05-16 14:19:13 +0100

    delete redirects if cart is non-empty , destroys if empty

--- app/controllers/cart_lines_controller.rb
+++ app/controllers/cart_lines_controller.rb
@@ -1,6 +1,6 @@
 class CartLinesController < ApplicationController
   include CurrentCart
-  before_action :set_current_cart, only: [:create]
+  before_action :set_current_cart, only: [:create, :destroy]
   before_action :set_cart_line, only: %i[ show edit update destroy ]
 
   # GET /cart_lines or /cart_lines.json
@@ -53,12 +53,20 @@ class CartLinesController < ApplicationController
   # DELETE /cart_lines/1 or /cart_lines/1.json
   def destroy
     @cart_line.destroy
-    respond_to do |format|
-      format.html { redirect_to cart_lines_url, notice: "Cart line was successfully destroyed." }
-      format.json { head :no_content }
+    if @cart.cart_lines.to_a.size > 0
+      respond_to do |format|
+        format.html { redirect_to @cart_line.cart, notice: "Cart line was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do|f|
+        f.html  do
+          redirect_to shop_products_url, notice: "cart is invalid and empty"
+        end
+        f.json { head :no_content}
     end
   end
-
+end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart_line
