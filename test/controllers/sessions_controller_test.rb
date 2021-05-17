a2e38b0b634f00f0baa2c5f9f11f1ee02601ebad
smeez <gatez@yahoo.com>
2021-05-17 08:32:35 +0100

    adds passed test cases for user sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
   test "should get new" do
-    get sessions_new_url
+    get login_url
     assert_response :success
   end
 
   test "should get create" do
-    get sessions_create_url
-    assert_response :success
+    post login_url, params: {username: @user.username, password: "arch_city"}
+    assert_response :redirect
+    assert_redirected_to admin_url
   end
 
   test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+    delete logout_path
+    assert_response :redirect
+    assert_redirected_to login_url
+    follow_redirect!
+    assert_equal flash[:notice], "you logged out"
   end
 
 end
