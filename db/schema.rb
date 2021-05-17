a0387e129c40cf8309ecfc7f19883c70ddc9a8d9
smeez <gatez@yahoo.com>
2021-05-17 01:06:43 +0100

    adds user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_072554) do
+ActiveRecord::Schema.define(version: 2021_05_16_234426) do
 
   create_table "cart_lines", force: :cascade do |t|
     t.integer "product_id", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2021_05_16_072554) do
     t.string "description"
     t.string "img"
     t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
