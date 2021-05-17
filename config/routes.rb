2af77af82edccd041cc8d48cb2b601bca812cbae
smeez <gatez@yahoo.com>
2021-05-17 12:56:01 +0100

    set shop_products_url as root

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   resources :cart_lines
   resources :carts
   
-  get 'shop/products', to: "catalog#index"
   resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "catalog#index", as: 'shop/products' 
+
+
 end
