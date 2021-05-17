cc73899bf56ac9b27e810a0e38e1db2fc42fad08
smeez <gatez@yahoo.com>
2021-05-17 08:46:55 +0100

    adds authorize before action call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
+    before_action :authorize_request
+
+    private
+    def authorize_request
+        unless User.find_by(id: session[:user_id])
+            redirect_to login_url, notice: "Please log in to Continue"
+        end
+    end
 end
