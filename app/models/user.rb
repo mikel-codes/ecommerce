73b9942efdadf82215c08004d2ed7416bd1c4683
smeez <gatez@yahoo.com>
2021-05-17 01:07:00 +0100

    adds unique validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
     validates :username, :password, presence: true
+    validates :username, uniqueness: true
+    has_secure_password
 
 end
