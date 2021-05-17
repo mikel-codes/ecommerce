4c210148e70dc8fde914458f87aaa810d8b94abf
smeez <gatez@yahoo.com>
2021-05-17 08:16:20 +0100

    adds actionlogggin in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,19 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def create 
+    u = User.find_by(username: params[:username])
+    if u.try(:authenticate, params[:password])
+      session[:user_id] =u.id
+      redirect_to admin_url, notice: "log in successfull"
+    else
+      redirect_to login_url, notice: "Invalid username and password"
+      
+    end
   end
 
   def destroy
+    session[:user_id]  = nil
+    redirect_to login_url, notice: "you logged out"
   end
 end
