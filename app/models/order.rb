5f0736f3f8925088b7e6bb78e592df1d01b45a97
smeez <gatez@yahoo.com>
2021-05-17 12:55:17 +0100

    changed payment_types to pay_types

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,16 @@
 class Order < ApplicationRecord
     has_many :cart_lines, dependent: :destroy
-    enum payment_types: {
+    enum pay_type: {
         "Check" => 0,
-        "Credit Card" => 1,
-        "Online Merchant" => 2
-    }
+        "Credit card" => 1,
+        "Purchase order" => 2
+        }
     validates :name, :email, :address, presence: true
-    validates :pay_type, inclusion: payment_types.keys
+    validates :pay_type, inclusion: pay_types.keys
+
+    def collect_all_items_from_cart(cart)
+        cart.cart_lines.each do |item|
+          cart_lines << item
+        end
+    end
 end
